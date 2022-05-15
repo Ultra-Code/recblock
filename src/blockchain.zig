cb1fd5cdec596a9e755e9b715ded38e618b6d8cd
Ultra-Code <mega.alpha100@gmail.com>
2022-05-15 00:53:27 +0000

    remove all use of algods from blockchain
    remove deinit fn's since no dynamic allocations are made in either Block or BlockChain now
    
    use log.info for printing state of chain instead of debug.print
    
    add a simple test case for Block
    
    Change structure of BlockChain struct since List isn't used anymore
    change api of newChain fn and simplify its implementation
    
    assert the new Blocks created are valid
    use lmdb for storing the blocks in the blockchain
    
    remove saveBlocks fn since blocks are now saved when created instead of
    at the end of the program
    
    Add a ChainIterator struct for iterating the blockchain from the latest
    to the oldest
    
    Remove BlockChain test since the components that make it up are tested
    and no actual new functionality is implemented in BlockChain that need
    to be tested

--- src/blockchain.zig
+++ src/blockchain.zig
@@ -1,18 +1,15 @@
 const std = @import("std");
 const mem = std.mem;
 const testing = std.testing;
-const print = std.debug.print;
 const panic = std.debug.panic;
-const debug = std.log.debug;
+const info = std.log.info;
 const crypto = std.crypto;
 const sha256 = crypto.hash.sha2.Sha256;
 const fmt = std.fmt;
+const assert = std.debug.assert;
 
 pub const Lmdb = @import("lmdb.zig").Lmdb;
 pub const BLOCK_DB = "blocks";
-
-const Algods = @import("algods");
-const List = Algods.linked_list.SinglyCircularList;
 
 pub fn reportOOM() noreturn {
     panic("allocator is Out Of Memory", .{});
@@ -149,93 +146,81 @@ pub const Block = struct {
         new_block.nonce = nonce;
         return new_block;
     }
-    pub fn deinit(_: Block) void {}
 };
 
 test "Block Test" {
-    var genesis_block = Block.genesisBlock(testing.allocator);
-    defer genesis_block.deinit();
-    const new_block = genesis_block.newBlock("test", genesis_block.hash);
-    defer new_block.deinit();
-    try testing.expectEqualSlices(u8, genesis_block.hash, new_block.previous_hash);
+    var genesis_block = Block.genesisBlock();
+    const new_block = Block.newBlock("testing Block".* ++ [_]u8{'0'} ** 19, genesis_block.hash);
+    try testing.expectEqualSlices(u8, genesis_block.hash[0..], new_block.previous_hash[0..]);
+    var hash: [32]u8 = undefined;
+    const nonce = new_block.POW(&hash);
+    try testing.expectEqual(nonce, new_block.nonce);
+    try testing.expectEqualStrings(hash[0..], new_block.hash[0..]);
 }
 
 //READ: https://en.bitcoin.it/wiki/Block_hashing_algorithm https://en.bitcoin.it/wiki/Proof_of_work https://en.bitcoin.it/wiki/Hashcash
+
+const LAST = "last";
 pub const BlockChain = struct {
-    blocks: List(Block),
+    last_hash: [32]u8,
     db: Lmdb,
 
-    pub fn newChain(arena: mem.Allocator, db: Lmdb, genesis_block: Block) BlockChain {
+    ///create a new BlockChain
+    pub fn newChain(db: Lmdb, genesis_block: Block) BlockChain {
         const txn = db.startTxn(.rw, BLOCK_DB);
         defer txn.commitTxns();
-        if (txn.getAs(Block, "last")) |last_block| {
-            var continue_chain_from_last_block = List(Block).init(arena);
-            continue_chain_from_last_block.append(last_block) catch reportOOM();
-            return .{ .blocks = continue_chain_from_last_block, .db = db };
+        if (txn.getAs([32]u8, LAST)) |last_block_hash| {
+            return .{ .last_hash = last_block_hash, .db = db };
         } else |err| switch (err) {
-            error.KeyNotInDb => {
-                var blocks = List(Block).init(arena);
-                blocks.append(genesis_block) catch reportOOM();
-                txn.put("last", genesis_block) catch unreachable;
-                return .{ .blocks = blocks, .db = db };
+            error.KeyNotFound => {
+                txn.put(genesis_block.hash[0..], genesis_block) catch unreachable;
+                txn.put(LAST, genesis_block.hash) catch unreachable;
+                return .{ .last_hash = genesis_block.hash, .db = db };
             },
-            error.InvalidParam => unreachable,
+            else => unreachable,
         }
     }
 
+    ///add a new Block to the BlockChain
     pub fn addBlock(bc: *BlockChain, block_data: [32]u8) void {
-        const previous_block = bc.blocks.last();
-        print("\nMining the block containing - '{s}'\n", .{block_data[0..]});
-        const new_block = Block.newBlock(block_data, previous_block.hash);
-        print("previous hash is '{}'\n", .{fmt.fmtSliceHexUpper(previous_block.hash[0..])});
-        print("'{s}' - has a valid hash of '{}'\n", .{ block_data[0..], fmt.fmtSliceHexUpper(new_block.hash[0..]) });
-        print("nonce is {}\n", .{new_block.nonce});
-        print("POW: {}\n\n", .{new_block.validate()});
-        bc.blocks.append(new_block) catch reportOOM();
-    }
+        info("Mining the block containing - '{s}'", .{block_data[0..]});
+        const new_block = Block.newBlock(block_data, bc.last_hash);
+        info("'{s}' - has a valid hash of '{}'", .{ block_data[0..], fmt.fmtSliceHexUpper(new_block.hash[0..]) });
+        info("nonce is {}", .{new_block.nonce});
+        info("POW: {}", .{new_block.validate()});
+        assert(new_block.validate() == true);
 
-    //TODO:Make iterator continue from where it left of previously
-    //maybe add size to List to track end
-    pub fn saveBlocks(bc: BlockChain) void {
-        // const chain = @bitCast([@sizeOf(@TypeOf(bc))]u8, bc);
-        // bc.db.put(bc.db.db_handle, block.hash, block) catch unreachable;
-        var txn = bc.db.startTxn(.rw, BLOCK_DB);
+        const txn = bc.db.startTxn(.rw, BLOCK_DB);
         defer txn.commitTxns();
 
-        var iter = bc.blocks.iterator();
-        while (iter.next()) |block| {
-            //TODO: improve and prevent key collision so that put could be catch unreachable
-            txn.put(block.hash[0..], block) catch |err| switch (err) {
-                error.KeyExist => {},
-                else => unreachable,
-            };
-        }
-        txn.update("last", bc.blocks.last()) catch unreachable;
-    }
-
-    pub fn deinit(bc: BlockChain) void {
-        bc.saveBlocks();
-        bc.blocks.deinit();
+        txn.put(new_block.hash[0..], new_block) catch unreachable;
+        txn.update(LAST, new_block.hash) catch unreachable;
+        bc.last_hash = new_block.hash;
     }
 };
 
-test "Blockchain Test" {
-    var allocator = testing.allocator;
-    var genesis_block = Block.genesisBlock(allocator);
-    var bc = BlockChain.newChain(allocator, genesis_block);
-    defer bc.deinit();
+pub const ChainIterator = struct {
+    db: *const Lmdb,
+    //Notice that an iterator initially points at the tip of a blockchain, thus blocks will be obtained from top to bottom, from newest to oldest.
+    current_hash: [32]u8,
 
-    bc.addBlock("transfer 1BTC to Esteban");
-    bc.addBlock("transfer 9BTC to Assan");
-
-    var iter = bc.blocks.iterator();
-    //TODO:work on converting hashes to Big endian which is usually the expected form for display
-    while (iter.next()) |block| {
-        print("\nprevious hash is '{}'\n", .{fmt.fmtSliceHexUpper(block.previous_hash)});
-        print("data is '{s}'\n", .{block.data});
-        print("current hash of {s} is '{}'\n", .{ block.data, fmt.fmtSliceHexUpper(block.hash) });
-        print("nonce is {}\n", .{block.nonce});
-        print("POW: {}\n", .{block.validate()});
+    pub fn iterator(bc: BlockChain) ChainIterator {
+        return .{ .db = &bc.db, .current_hash = bc.last_hash };
     }
-    return error.SkipZigTest;
-}
+
+    ///the returned usize is the address of the Block in memory
+    ///the ptr can be obtained with @intToPtr
+    pub fn next(self: *ChainIterator) ?Block {
+        const txn = self.db.startTxn(.ro, BLOCK_DB);
+        defer txn.doneReading();
+
+        if (txn.getAs(Block, self.current_hash[0..])) |current_block| {
+            self.current_hash = current_block.previous_hash;
+
+            return current_block;
+            // return @ptrToInt(current_block);
+        } else |_| {
+            return null;
+        }
+    }
+};
