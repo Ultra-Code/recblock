ba0b70d21fc051da6bd42d4344c29ea972f18250
Ultra-Code <mega.alpha100@gmail.com>
2022-05-15 01:19:55 +0000

    simplify initialization of initdb
    Remove allocators since no allocations are made
    use the new newChain api
    
    use ChainIterator for iterating the blockchain
    use info for logging information

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const fmt = std.fmt;
-const print = std.debug.print;
+const info = std.log.info;
 
 const blockchain = @import("blockchain.zig");
 const Block = blockchain.Block;
@@ -8,25 +8,29 @@ const BlockChain = blockchain.BlockChain;
 const Lmdb = blockchain.Lmdb;
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    var arena_alloc = std.heap.ArenaAllocator.init(gpa.allocator());
-    const allocator = arena_alloc.allocator();
-    defer arena_alloc.deinit();
+    var db_env = Lmdb.initdb("./db", .rw);
+    defer db_env.deinitdb();
 
-    const HOME = std.os.getenv("HOME").?;
-    var dir_buf: [64]u8 = undefined;
-    const db_dir = fmt.bufPrintZ(&dir_buf, "{[HOME]s}/{[db_dir]s}", .{
-        .HOME = HOME,
-        .db_dir = "repos/zig/recblock/db/",
-    }) catch unreachable;
-
-    var db = Lmdb.initdb(db_dir, .rw);
-    defer db.deinitdb();
-
-    var genesis_block = Block.genesisBlock();
-    var bc = BlockChain.newChain(allocator, db, genesis_block);
-    defer bc.deinit();
+    const genesis_block = Block.genesisBlock();
+    var bc = BlockChain.newChain(db_env, genesis_block);
 
     bc.addBlock("transfer 1BTC to Esteban 2638219".*);
     bc.addBlock("transfer 9BTC to Assan 238981183".*);
+
+    var chain_iter = blockchain.ChainIterator.iterator(bc);
+    //TODO:work on converting hashes to Big endian which is usually the expected form for display
+    info("start iteration", .{});
+    while (chain_iter.next()) |current_block| {
+        // const current_block = @intToPtr(*Block, block);
+        info("previous hash is '{X}'", .{fmt.fmtSliceHexUpper(current_block.previous_hash[0..])});
+        info("data is '{s}'", .{current_block.data});
+        info("current hash of {s} is '{X}'", .{ current_block.data, fmt.fmtSliceHexUpper(current_block.hash[0..]) });
+        info("nonce is {}", .{current_block.nonce});
+        info("POW: {}", .{current_block.validate()});
+    }
+    info("done", .{});
+}
+
+test {
+    std.testing.refAllDecls(@This());
 }
