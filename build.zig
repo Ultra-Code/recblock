d9dac7daacf18aa19103f571205e04b6073ca751
Ultra-Code <mega.alpha100@gmail.com>
2024-09-11 00:13:08 +0000

    Fix recblock build on zig 0.14.0-dev
    This depends on getting s2s fix on this zig version

--- build.zig
+++ build.zig
@@ -1,16 +1,9 @@
 const std = @import("std");
 const builtin = @import("builtin");
-const Pkg = std.build.Pkg;
-const pkgs = struct {
-    const s2s = Pkg{
-        .name = "s2s",
-        .source = .{ .path = "./deps/s2s/s2s.zig" },
-        .dependencies = &[_]Pkg{},
-    };
-};
+
 const LMDB_PATH = "./deps/lmdb/libraries/liblmdb/";
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -19,48 +12,78 @@ pub fn build(b: *std.build.Builder) void {
 
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize =
+        b.standardOptimizeOption(
+        .{ .preferred_optimize_mode = .ReleaseSafe },
+    );
 
     //Add lmdb library for embeded key/value store
     const cflags = [_][]const u8{ "-pthread", "-std=c2x" };
-    const lmdb_sources = [_][]const u8{ LMDB_PATH ++ "mdb.c", LMDB_PATH ++ "midl.c" };
-    const lmdb = b.addStaticLibrary("lmdb", null);
-    lmdb.setTarget(target);
-    lmdb.setBuildMode(mode);
-    lmdb.addCSourceFiles(&lmdb_sources, &cflags);
+    const lmdb_sources = [_][]const u8{
+        LMDB_PATH ++ "mdb.c",
+        LMDB_PATH ++ "midl.c",
+    };
+    const lmdb = b.addStaticLibrary(.{
+        .name = "lmdb",
+        .target = target,
+        .optimize = optimize,
+    });
+    lmdb.addCSourceFiles(.{
+        .files = &lmdb_sources,
+        .flags = &cflags,
+    });
     lmdb.linkLibC();
-    lmdb.install();
+    const install_lmdb = b.addInstallArtifact(
+        lmdb,
+        .{},
+    );
+    b.getInstallStep().dependOn(&install_lmdb.step);
 
-    const target_name = target.allocDescription(b.allocator) catch unreachable;
-    const exe_name = std.fmt.allocPrint(b.allocator, "{[program]s}-{[target]s}", .{ .program = "recblock", .target = target_name }) catch unreachable;
+    const target_name = target.query.allocDescription(b.allocator) catch unreachable;
+    const exe_name = std.fmt.allocPrint(
+        b.allocator,
+        "{[program]s}-{[target]s}",
+        .{
+            .program = "recblock",
+            .target = target_name,
+        },
+    ) catch unreachable;
 
-    const exe = b.addExecutable(exe_name, "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.addPackage(pkgs.s2s);
+    const exe = b.addExecutable(.{
+        .name = exe_name,
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const s2s_dep = b.dependency("s2s", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const s2s_module = s2s_dep.module("s2s");
+    exe.root_module.addImport("s2s", s2s_module);
     exe.linkLibrary(lmdb);
-    exe.addIncludePath(LMDB_PATH);
-    exe.install();
+    exe.addIncludePath(b.path(LMDB_PATH));
+    b.installArtifact(exe);
 
-    switch (mode) {
+    switch (optimize) {
         .Debug => {},
         else => {
             lmdb.link_function_sections = true;
-            lmdb.red_zone = true;
+            lmdb.root_module.red_zone = true;
             lmdb.want_lto = true;
 
             exe.link_function_sections = true;
-            exe.red_zone = true;
+            exe.root_module.red_zone = true;
             exe.want_lto = true;
             //FIXME: cross compiling for windows with strip run into issues
             //Wait for self-hosting and if problem still persist,open an issue to track this
-            if (!target.isWindows()) {
-                lmdb.strip = true;
-                exe.strip = true;
+            if (builtin.target.os.tag != .windows) {
+                lmdb.root_module.strip = true;
+                exe.root_module.strip = true;
             }
         },
     }
-    const run_cmd = exe.run();
+    const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
@@ -70,12 +93,12 @@ pub fn build(b: *std.build.Builder) void {
     run_step.dependOn(&lmdb.step);
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-    exe_tests.addPackage(pkgs.s2s);
+    const exe_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+    });
+    exe_tests.root_module.addImport("s2s", s2s_module);
     exe_tests.linkLibrary(lmdb);
-    exe_tests.addIncludePath(LMDB_PATH);
+    exe_tests.addIncludePath(b.path(LMDB_PATH));
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&lmdb.step);
